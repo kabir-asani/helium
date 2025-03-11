3f58d8be4c84b081332b6cd19a58578f34d052f4
Kabir Asani <iamkabirasani@gmail.com>
2025-03-11 18:42:44 +0530

    Add API endpoints for random number generation, current time, environment info, and number storage

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,73 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+serve(app);
+
+app.get("/generate", (context) => {
+  return context.json(
+    {
+      randomNumber: Math.random(),
+    },
+    200
+  );
 });
 
-serve(
-  {
-    fetch: app.fetch,
-    port: 3000,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`);
-  }
-);
+app.get("/current-time", (context) => {
+  const date = new Date();
+
+  return context.json(
+    {
+      currentTime: date.toLocaleString(),
+    },
+    200
+  );
+});
+
+app.get("/environment", (context) => {
+  const currentNodeVersion = process.version;
+  const currentPlatform = process.platform;
+
+  return context.json(
+    {
+      version: currentNodeVersion,
+      platform: currentPlatform,
+    },
+    200
+  );
+});
+
+app.get("/puppet", (context) => {
+  const queryParameters = context.req.query();
+
+  return context.json(
+    {
+      queryParameters,
+    },
+    200
+  );
+});
+
+const numbers: number[] = [];
+
+app.post("/numbers", async (context) => {
+  const body = await context.req.json();
+  const number = body.number;
+
+  numbers.push(number);
+
+  return context.json(
+    {
+      storedNumber: number,
+    },
+    200
+  );
+});
+
+app.get("/numbers", (context) => {
+  return context.json(
+    {
+      numbers,
+    },
+    200
+  );
+});
